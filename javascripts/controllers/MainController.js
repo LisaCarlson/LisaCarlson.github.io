f03cbe09c0a3f58ef4023adae7d1f1f9868851d2
Lisa Carlson <lisa.carlson85@gmail.com>
2017-11-09 14:33:30 -0700

    styling and content

--- javascripts/controllers/MainController.js
+++ javascripts/controllers/MainController.js
@@ -1,3 +1,19 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.welcome = "Hello World";
+  $scope.projects = [
+    {
+      title: 'Human Design',
+      img: './images/hd.png',
+      url: 'https://humandesign.com/'
+    }, 
+    {
+      title: 'MTHRship',
+      img: './images/mthrship.png',
+      url: 'https://mthrship.com/'
+    }, 
+    {
+      title: 'Wanderlust',
+      img: './images/wanderlust.png',
+      url: 'https://wanderlust-trails.herokuapp.com'
+    }
+  ];
 }]);
