325c0ee7fafa42c33f001227d2ed6fdebf74eb4a
Lisa Carlson <lisa.carlson85@gmail.com>
2017-11-09 20:21:42 -0700

    routes work

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,8 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'MainController'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'MainController'});
+  $routeProvider.otherwise({redirectTo: '/'});
+  $locationProvider.html5Mode(true);
+}])
--- javascripts/controllers/MainController.js
+++ javascripts/controllers/MainController.js
@@ -3,17 +3,23 @@ app.controller('MainController', ['$scope', function($scope) {
     {
       title: 'Human Design',
       img: './images/hd.png',
-      url: 'https://humandesign.com/'
+      url: 'https://humandesign.com/',
+      type: 'Work Project',
+      description: 'A fully responsive site, combining custom interactive javascript and visual effects.'
     }, 
     {
       title: 'MTHRship',
       img: './images/mthrship.png',
-      url: 'https://mthrship.com/'
+      url: 'https://mthrship.com/',
+      type: 'Work Project',
+      description: 'A fully responsive site where companies or individuals can pitch investment ideas to Human Design. '
     }, 
     {
       title: 'Wanderlust',
       img: './images/wanderlust.png',
-      url: 'https://wanderlust-trails.herokuapp.com'
+      url: 'https://wanderlust-trails.herokuapp.com',
+      type: 'Side Project',
+      description: 'An app that provides weather and hiking trail information for the user\'s location.'
     }
   ];
 }]);
